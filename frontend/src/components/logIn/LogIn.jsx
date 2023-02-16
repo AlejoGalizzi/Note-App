171014384a3be81b5a19bc1fe0419954386cbc34
Alejo Galizzi <alejo.galizzi@jala.university>
2023-02-16 19:07:30 -0300

    Resize login page.

--- frontend/src/components/logIn/LogIn.jsx
+++ frontend/src/components/logIn/LogIn.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import { TextField, Button } from '@mui/material';
-import { logIn } from '../../api/configRequest';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { TextField, Button } from "@mui/material";
+import { logIn } from "../../api/configRequest";
+import { useNavigate } from "react-router-dom";
+import { Container } from "@mui/system";
 
 function LogIn() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate()
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -19,32 +20,34 @@ function LogIn() {
   const handleSubmit = (event) => {
     event.preventDefault();
     logIn(username, password).then((response) => {
-      localStorage.setItem("token", response.data.token)
-      navigate("/")
-    })
+      localStorage.setItem("token", response.data.token);
+      navigate("/");
+    });
   };
 
   return (
-    <form onSubmit={(event) => handleSubmit(event)}>
-      <TextField
-        label="Username"
-        value={username}
-        onChange={handleUsernameChange}
-        margin="normal"
-        fullWidth
-      />
-      <TextField
-        label="Password"
-        type="password"
-        value={password}
-        onChange={handlePasswordChange}
-        margin="normal"
-        fullWidth
-      />
-      <Button type="submit" variant="contained" color="primary">
-        Log in
-      </Button>
-    </form>
+    <Container>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <TextField
+          label="Username"
+          value={username}
+          onChange={handleUsernameChange}
+          margin="normal"
+          fullWidth
+        />
+        <TextField
+          label="Password"
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          margin="normal"
+          fullWidth
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Log in
+        </Button>
+      </form>
+    </Container>
   );
 }
 
