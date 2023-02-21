1d12d80567371dc682f0b724615f4813894845c1
Alejo Galizzi <alejo.galizzi@jala.university>
2023-02-21 15:25:51 -0300

    Fix some ui errors. And modify to generalize some deploy process.

--- backend/src/main/java/com/alejogalizzi/notes/config/CorsConfiguration.java
+++ backend/src/main/java/com/alejogalizzi/notes/config/CorsConfiguration.java
@@ -1,5 +1,8 @@
 package com.alejogalizzi.notes.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,15 +10,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration {
+  Logger logger = LoggerFactory.getLogger(CorsConfiguration.class);
+  @Value("${FRONTEND_URL}")
+    private String frontendUrl;
 
   @Bean
   public WebMvcConfigurer corsConfigurer() {
+    logger.info(frontendUrl);
+
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost")
-            .allowedMethods("GET", "POST", "PUT", "DELETE");
+            .allowCredentials(true)
+            .allowedOrigins(frontendUrl)
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .exposedHeaders("Access-Control-Allow-Origin");
       }
     };
   }
