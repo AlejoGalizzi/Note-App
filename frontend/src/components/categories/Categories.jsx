de2f7d58e8cdd719462236e1986474b31a79586b
Alejo Galizzi <81248632+AlejoGalizzi@users.noreply.github.com>
2023-03-03 18:31:40 -0300

    Refactor code to reuse menu button in all the project
    * Add categories implementation to the project, to set a more proper crud of it's entity.
    
    * Add implementation to add a color to a category and also create a page to see all categories in one page and the possibility to erase them.
    
    * Add button to navigate to categories page.
    
    * Refactor menu from topbar.
    
    * Refactor menu's button to set it more reusable.
    
    * Modify conditional render in menuButton component. Also format some code with prettier.
    
    * Fix import.
    
    ---------
    
    Co-authored-by: Alejo Galizzi <alejo.galizzi@jala.university>

--- frontend/src/components/categories/Categories.jsx
+++ frontend/src/components/categories/Categories.jsx
@@ -1,33 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { AppBar, Avatar, Box, Button, Grid, Icon, IconButton, List, ListItem, ListItemAvatar, ListItemText, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Grid,
+  Icon,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { deleteCategory, getCategories } from "../../api/configRequest";
 import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
-import DensityMediumIcon from "@mui/icons-material/DensityMedium";
-import BookmarkIcon from '@mui/icons-material/Bookmark';
+import BookmarkIcon from "@mui/icons-material/Bookmark";
 import { DeleteOutline } from "@mui/icons-material";
 import { Container } from "@mui/system";
-import { Link, useNavigate } from "react-router-dom";
-import ConfirmationModal from "../confirmationModal/ConfirmationModal";
 import MenuButton from "../menuButton/MenuButton";
+import ConfirmationModal from "../confirmationModal/ConfirmationModal";
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [selectedCategory, setSelectedCategory] = useState({})
-  const [ openDeleteModal, setOpenDeleteModal ] = useState(false);
-  const navigate = useNavigate();
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleLogOut = () => {
-    localStorage.removeItem('token');
-    navigate("/login");
-  }
+  const [selectedCategory, setSelectedCategory] = useState({});
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
 
   const onHandleDelete = () => {
     deleteCategory(selectedCategory.id).then(() => {
@@ -42,7 +38,7 @@ const Categories = () => {
     });
     setSelectedCategory(null);
     setOpenDeleteModal(false);
-  }
+  };
 
   const renderCategoriesAPI = () => {
     getCategories()
@@ -51,7 +47,7 @@ const Categories = () => {
   };
 
   useEffect(() => {
-    renderCategoriesAPI()
+    renderCategoriesAPI();
   }, []);
 
   const renderCategories = () => {
@@ -78,91 +74,52 @@ const Categories = () => {
       <ListItem
         key={index}
         secondaryAction={
-          <IconButton edge="end" aria-label="delete" onClick={() => {
-            setSelectedCategory(category);
-            setOpenDeleteModal(true);
-          }}>
+          <IconButton
+            edge="end"
+            aria-label="delete"
+            onClick={() => {
+              setSelectedCategory(category);
+              setOpenDeleteModal(true);
+            }}
+          >
             <DeleteOutline />
           </IconButton>
         }
       >
         <ListItemAvatar key={index}>
-            <BookmarkIcon sx={{ color: category.color, backgroundColor: category }} fontSize="large"/>
+          <BookmarkIcon
+            sx={{ color: category.color, backgroundColor: category }}
+            fontSize="large"
+          />
         </ListItemAvatar>
         <ListItemText primary={category.name} />
       </ListItem>
     ));
   };
 
-  return <><Container>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-              Categories
-            </Typography>
-            {/* <Button
-              id="basic-button"
-              aria-controls={open ? "basic-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-              onClick={handleClick}
-              sx={{ color: "white" }}
-            >
-              <DensityMediumIcon color="white" />
-            </Button>
-
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                "aria-labelledby": "basic-button",
-              }}
-            >
-              <MenuItem>
-                <Link to="/home">
-                  <Button
-                    id="fade-button"
-                    aria-controls={true ? "fade-menu" : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={true ? "true" : undefined}
-                    onClick={() => {}}
-                  >
-                    <Typography variant="p" component="p" color="black">
-                      HOME
-                    </Typography>
-                  </Button>
-                </Link>
-              </MenuItem>
-              <MenuItem>
-                <Button
-                  id="fade-button"
-                  aria-controls={true ? "fade-menu" : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={true ? "true" : undefined}
-                  onClick={handleLogOut}
-                >
-                  <Typography variant="p" component="p" color="black">
-                    Log Out
-                  </Typography>
-                </Button>
-              </MenuItem>
-            </Menu> */}
-            <MenuButton/>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <List >{renderCategories()}</List>
-    </Container>
-    <ConfirmationModal
-      open={openDeleteModal}
-      setOpen={setOpenDeleteModal}
-      handleConfirm={onHandleDelete}
-      setObject={setSelectedCategory}
-    />
-    </>;
+  return (
+    <>
+      <Container>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                Categories
+              </Typography>
+              <MenuButton />
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <List>{renderCategories()}</List>
+      </Container>
+      <ConfirmationModal
+        open={openDeleteModal}
+        setOpen={setOpenDeleteModal}
+        handleConfirm={onHandleDelete}
+        setObject={setSelectedCategory}
+      />
+    </>
+  );
 };
 
 export default Categories;
